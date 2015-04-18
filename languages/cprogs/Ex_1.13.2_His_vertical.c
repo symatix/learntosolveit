c5f53ef68a228a84bf15a74cf013098574d62ffb
symatix <pockshocks@gmail.com>
2015-04-19 00:59:51 +0200

    Update Ex_1.13.2_His_vertical.c
    The exercise is not to replace all characters as 'X' and print them as a vertical histogram, but to count words and put them in histogram columns corresponding to their word length.

--- languages/cprogs/Ex_1.13.2_His_vertical.c
+++ languages/cprogs/Ex_1.13.2_His_vertical.c
@@ -1,43 +1,57 @@
-/* Vertical Histogram of words in a Sentence */
-
-#include<stdio.h>
-#define MAXWL 20  /* Maximum length of a word */
-#define MAXNO 25 /* Maximum No of words in a sentence */
-
-int main(void)
+// Create vertical histogram of word lengths
+#include <stdio.h>
+#define MWN 100
+int main()
 {
-	int word[MAXNO];
-	int i,c,j,nc,nw;
-
-	for(i=0;i<MAXNO;++i)
-		word[i]=0;
-
-	nc = nw = 0;
-
-	while( (c=getchar()) != EOF)
-	{
-		++nc;
-		if( c ==' ' || c =='\n' || c =='\t')
-		{
-			word[nw] = nc -1; /* -1 for excluding the space in the word length */
-			++nw;
-			nc = 0; /* resetting the word-length for the next word */
-		}
-	
-	}
-
-	for( i = MAXWL; i >= 1; --i)
-	{
-		for(j=0;j <= nw;++j)
-		{
-			if( i <= word[j])
-				putchar('*');
-			else	
-				putchar(' ');
-			}
-	putchar('\n');
-	}
-
-return 0;
+    int c;
+    int hh, hl, nc;                                 // histogram hight, length and letter counter
+    int i, j;
+    int word[MWN];
+    
+    hl = 0;
+    hh = 0;
+    nc = 0;
+    
+    for (i = 0; i < MWN; ++i)                       // initialize array to 0
+        word[i]=0;
+                                            
+                                                    // this part adds values to array
+    while ((c=getchar()) != EOF){
+        ++nc;
+        if (c == ' ' || c == '\n' || c == '\t'){
+            nc = nc - 1;
+            word[nc]= word[nc] + 1;                 // adds +1 to array(letter number of the word -blank space)
+            if (word[nc] > hh){
+                hh = word[nc];                      // determens height of histogram
+            }
+            if (nc > hl){                           // determines length of histogram
+                hl = nc + 1;
+            }
+            nc=0;                                   // resets counter for next word
+        }
+    }
+    
+    printf("\n\n");                 
+    
+    
+                                                    // this part prints the histogram
+    for (i = hh; i >= 1; --i){                      // go from highest value in array to 1
+        printf("%3d |", i);                         // prints vertical part of histogram
+        for (j = 1; j < hl; ++j){
+            if (i <= word[j])                       // if the value in array is equal or bigger than
+                printf(" X ");                      // 'i' - print X
+            else                                    // else print blank characters
+                printf("   ");
+        }
+        
+        putchar('\n');
+            
+    }                                               // this part prints horizontal border of the histagram
+    printf("    +");
+    for (i=1;i<=hl-1; ++i)
+        printf("---");
+    putchar('\n');
+    printf("    ");
+    for (i=1; i<=hl-1; ++i)
+        printf("%3d",i);
 }
-
